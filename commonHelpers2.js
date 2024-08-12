3525a376990be651ecacce511e30c2bfb54a3671
Vitalii-Artiukh <radial_car@yahoo.com>
2024-08-12 21:57:05 +0000

    Deploying to gh-pages from @ Vitalii-Artiukh/goit-js-hw-10@a38cdcacfb38295d335504e9f6af118fd176d3d4 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-77e16229.js";o.show({message:"What would you like to add?",messageColor:"#ffffff",backgroundColor:"red",closeOnEscape:!0,position:"topRight",timeout:2e4});
+//# sourceMappingURL=commonHelpers2.js.map
